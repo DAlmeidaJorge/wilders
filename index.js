7e2c62c3d1ca76d00a4dd4f9c134e38f2b363112
DAlmeidaJorge <duartejorge_93@hotmail.com>
2023-03-20 16:38:31 +0000

    wildersworkshop

---
+++ index.js
@@ -0,0 +1,10 @@
+
+import chalk from "chalk";
+const name1 = "Josélio";
+const name2 = "Nuno";
+const name3 = "Sjoerd";
+const name4 = "Claudia";
+console.log(chalk.blue(name1));
+console.log(chalk.red(name2));
+console.log(chalk.green(name3));
+console.log(chalk.yellow(name4));
